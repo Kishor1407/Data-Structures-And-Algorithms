5fefb4e344d2526e06eb1ec2bdf3fda1160ba9b0
Kishor Hegge <93866205+Kishor1407@users.noreply.github.com>
2026-01-07 10:36:46 +0530

    Time: 0 ms (100%), Space: 54.9 MB (71.68%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.js
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.js
@@ -6,29 +6,75 @@
 var searchRange = function(arr, target) {
     let l=0;
     let r=arr.length-1;
-    let ans = [-1,-1]
+    let ans =[-1,-1];
 
-    while(l<r){
-        let m=l+Math.floor((r-l)/2);
-
-        if(arr[m]<target){
+    while(l<=r){
+        let m = l+Math.floor((r-l)/2)
+        if(arr[m] === target){
+            ans[0]=m;
+            r=m-1;
+        }
+        else if(arr[m]<target){
             l=m+1;
-        }else{
-            r=m
+        }
+        else{
+            r=m-1;
         }
     }
-    if(arr[l] === target) ans[0] =l;
-    
     l=0;
     r=arr.length-1;
 
-    while(l<r){
-        let m=l+Math.ceil((r-l)/2);
-        if(arr[m]>target) r=m-1;
-        else l =m;
+    while(l<=r){
+        let m = l+Math.floor((r-l)/2)
+        if(arr[m] === target){
+            ans[1]=m;
+            l=m+1;
+        }
+        else if(arr[m]<target){
+            l=m+1;
+        }
+        else{
+            r=m-1;
+        }
     }
-    if(arr[l] === target) ans[1] = l;
-
-    return ans;
-
+   return ans;
+    
 };
+
+
+
+
+// /**
+//  * @param {number[]} nums
+//  * @param {number} target
+//  * @return {number[]}
+//  */
+// var searchRange = function(arr, target) {
+//     let l=0;
+//     let r=arr.length-1;
+//     let ans = [-1,-1]
+
+//     while(l<r){
+//         let m=l+Math.floor((r-l)/2);
+
+//         if(arr[m]<target){
+//             l=m+1;
+//         }else{
+//             r=m
+//         }
+//     }
+//     if(arr[l] === target) ans[0] =l;
+    
+//     l=0;
+//     r=arr.length-1;
+
+//     while(l<r){
+//         let m=l+Math.ceil((r-l)/2);
+//         if(arr[m]>target) r=m-1;
+//         else l =m;
+//     }
+//     if(arr[l] === target) ans[1] = l;
+
+//     return ans;
+
+// };
