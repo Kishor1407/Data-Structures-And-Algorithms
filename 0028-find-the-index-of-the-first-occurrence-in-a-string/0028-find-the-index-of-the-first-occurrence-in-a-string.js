67cfc60c5ff1b99ecf2bdb85777270e9b11935fa
Kishor Hegge <93866205+Kishor1407@users.noreply.github.com>
2026-01-09 11:07:38 +0530

    Time: 2 ms (23.49%), Space: 54.5 MB (33.02%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
@@ -4,5 +4,20 @@
  * @return {number}
  */
 var strStr = function(haystack, needle) {
-    return haystack.indexOf(needle);
+    // return haystack.indexOf(needle);
+
+    let n=haystack.length;
+    let m=needle.length;
+    let i=j=0;
+    for(i=0;i<=n-m;i++){
+        for(j=0;j<m;j++){
+            if(haystack[i+j] !== needle[j]){
+                break;
+            }
+        }
+        if(j===m){
+            return i;
+        }
+    }
+    return -1;
 };
