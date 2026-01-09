98e2a85e11f3db2524cded40df7647a5163d4746
Kishor Hegge <93866205+Kishor1407@users.noreply.github.com>
2026-01-09 10:20:45 +0530

    Time: 8 ms (44.68%), Space: 56.5 MB (39.43%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -1,15 +1,37 @@
+// /**
+//  * @param {number[]} nums
+//  * @param {number} target
+//  * @return {number[]}
+//  */
+// var twoSum = function(nums, target) {
+//     let ans=0;
+//     for(let i=0;i<nums.length;i++){
+//         for(let j=i+1;j<nums.length;j++){
+//             if(nums[i]+nums[j]===target){
+//                 return [i,j];
+//             }
+//         }
+//     }
+    
+// };
+
 /**
  * @param {number[]} nums
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
-    let ans=0;
-    for(let i=0;i<nums.length;i++){
-        for(let j=i+1;j<nums.length;j++){
-            if(nums[i]+nums[j]===target){
-                return [i,j];
-            }
+var twoSum = function(arr, target) {
+    let n=arr.length;
+
+    let map={};
+    for(let i=0;i<n;i++){
+        map[arr[i]]=i;
+    }
+
+    for(let i=0;i<n;i++){
+        let pairToFind=target-arr[i];
+        if(map[pairToFind]  !== undefined  && map[pairToFind] !== i){
+            return [i,map[pairToFind]]
         }
     }
     
