c920ce559e770195b56f0b676d436a7ab05182fb
Kishor Hegge <93866205+Kishor1407@users.noreply.github.com>
2026-01-12 11:05:07 +0530

    Time: 9 ms (12.21%), Space: 55.9 MB (11.94%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
@@ -4,20 +4,76 @@
  * @return {number}
  */
 var strStr = function(haystack, needle) {
-    // return haystack.indexOf(needle);
+    if (needle.length === 0) return 0;
 
-    let n=haystack.length;
-    let m=needle.length;
-    let i=j=0;
-    for(i=0;i<=n-m;i++){
-        for(j=0;j<m;j++){
-            if(haystack[i+j] !== needle[j]){
-                break;
+    const n = haystack.length;
+    const m = needle.length;
+
+    // Step 1: Build LPS array
+    const lps = new Array(m).fill(0);
+    let len = 0; // length of previous longest prefix suffix
+    let i = 1;
+
+    while (i < m) {
+        if (needle[i] === needle[len]) {
+            len++;
+            lps[i] = len;
+            i++;
+        } else {
+            if (len !== 0) {
+                len = lps[len - 1];
+            } else {
+                lps[i] = 0;
+                i++;
             }
         }
-        if(j===m){
-            return i;
+    }
+
+    // Step 2: KMP search
+    let h = 0; // index for haystack
+    let p = 0; // index for needle
+
+    while (h < n) {
+        if (haystack[h] === needle[p]) {
+            h++;
+            p++;
+        }
+
+        if (p === m) {
+            return h - p; // match found
+        } else if (h < n && haystack[h] !== needle[p]) {
+            if (p !== 0) {
+                p = lps[p - 1];
+            } else {
+                h++;
+            }
         }
     }
+
     return -1;
 };
+
+
+// /**
+//  * @param {string} haystack
+//  * @param {string} needle
+//  * @return {number}
+//  */
+// var strStr = function(haystack, needle) {
+//     // return haystack.indexOf(needle);
+
+//     let n=haystack.length;
+//     let m=needle.length;
+//     let i=j=0;
+//     for(i=0;i<=n-m;i++){
+//         for(j=0;j<m;j++){
+//             if(haystack[i+j] !== needle[j]){
+//                 break;
+//             }
+//         }
+//         if(j===m){
+//             return i;
+//         }
+//     }
+//     return -1;
+// };
