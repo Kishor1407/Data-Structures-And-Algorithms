9b13c9ac331e5b7f0d7e8055ee677bfb78637685
Kishor Hegge <93866205+Kishor1407@users.noreply.github.com>
2026-01-15 16:48:22 +0530

    Time: 0 ms (100%), Space: 53.8 MB (78.08%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.js
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.js
@@ -11,15 +11,46 @@
  * @return {number[]}
  */
 var inorderTraversal = function(root) {
-let ans=[];
-function traversal(curr){
-    if(!curr) return;
-    traversal(curr.left);
-    ans.push(curr.val);
-    traversal(curr.right);
-}
+    let ans=[];
+    let stack=[];
+    let curr=root;
+    while(curr || stack.length){
+    while(curr){
+        stack.push(curr);
+        curr=curr.left;
 
-traversal(root);
-return ans;
-    
+    }
+    curr=stack.pop();
+    ans.push(curr.val);
+    curr=curr.right;
+    }
+    return ans;
 };
+
+
+
+// /**
+//  * Definition for a binary tree node.
+//  * function TreeNode(val, left, right) {
+//  *     this.val = (val===undefined ? 0 : val)
+//  *     this.left = (left===undefined ? null : left)
+//  *     this.right = (right===undefined ? null : right)
+//  * }
+//  */
+// /**
+//  * @param {TreeNode} root
+//  * @return {number[]}
+//  */
+// var inorderTraversal = function(root) {
+// let ans=[];
+// function traversal(curr){
+//     if(!curr) return;
+//     traversal(curr.left);
+//     ans.push(curr.val);
+//     traversal(curr.right);
+// }
+
+// traversal(root);
+// return ans;
+    
+// };
