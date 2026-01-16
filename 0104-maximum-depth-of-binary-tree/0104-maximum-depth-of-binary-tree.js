a0835c1de2698ef57c5de3cf42d023cba22d6535
Kishor Hegge <93866205+Kishor1407@users.noreply.github.com>
2026-01-16 16:33:29 +0530

    Time: 0 ms (100%), Space: 56.6 MB (89.36%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.js
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.js
@@ -11,13 +11,35 @@
  * @return {number}
  */
 var maxDepth = function(root) {
-if(!root) return 0;
-    let maxDepth=0;
-    let traverse=(curr,depth)=>{
-        maxDepth=Math.max(maxDepth,depth);
-        curr.left && traverse(curr.left,depth+1);
-        curr.right && traverse(curr.right,depth+1);
-    }
-    traverse(root,1);
-    return maxDepth;
+    if(!root) return 0;
+    let leftMax=maxDepth(root.left);
+   let rightMax=maxDepth(root.right);
+    return 1+ Math.max(leftMax,rightMax)
 };
+
+
+
+
+// /**
+//  * Definition for a binary tree node.
+//  * function TreeNode(val, left, right) {
+//  *     this.val = (val===undefined ? 0 : val)
+//  *     this.left = (left===undefined ? null : left)
+//  *     this.right = (right===undefined ? null : right)
+//  * }
+//  */
+// /**
+//  * @param {TreeNode} root
+//  * @return {number}
+//  */
+// var maxDepth = function(root) {
+// if(!root) return 0;
+//     let maxDepth=0;
+//     let traverse=(curr,depth)=>{
+//         maxDepth=Math.max(maxDepth,depth);
+//         curr.left && traverse(curr.left,depth+1);
+//         curr.right && traverse(curr.right,depth+1);
+//     }
+//     traverse(root,1);
+//     return maxDepth;
+// };
