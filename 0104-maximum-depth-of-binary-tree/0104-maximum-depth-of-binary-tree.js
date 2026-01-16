0e0e61454326087459e81b379f48649ecc26b2cc
Kishor Hegge <93866205+Kishor1407@users.noreply.github.com>
2026-01-16 16:29:09 +0530

    Time: 2 ms (18.32%), Space: 56.7 MB (89.36%) - LeetHub

---
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.js
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function(root) {
+if(!root) return 0;
+    let maxDepth=0;
+    let traverse=(curr,depth)=>{
+        maxDepth=Math.max(maxDepth,depth);
+        curr.left && traverse(curr.left,depth+1);
+        curr.right && traverse(curr.right,depth+1);
+    }
+    traverse(root,1);
+    return maxDepth;
+};
