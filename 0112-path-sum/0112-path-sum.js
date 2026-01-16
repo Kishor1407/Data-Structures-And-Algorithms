65b870b41cc93b058aa21e1448245ee7cc09ff06
Kishor Hegge <93866205+Kishor1407@users.noreply.github.com>
2026-01-16 16:48:56 +0530

    Time: 1 ms (52.46%), Space: 56.3 MB (94.02%) - LeetHub

--- 0112-path-sum/0112-path-sum.js
+++ 0112-path-sum/0112-path-sum.js
@@ -12,18 +12,26 @@
  * @return {boolean}
  */
 var hasPathSum = function(root, targetSum) {
-    if(!root) return false;
-    let ans=false;
-    let traverse=(curr,currSum)=>{
-        let newSum=currSum+curr.val;
-        if(!curr.left && !curr.right){
-            if(newSum === targetSum){
-                ans=ans || true;
-            }
-        }
-            curr.left && traverse(curr.left,newSum);
-    curr.right && traverse(curr.right,newSum); 
-    }
-traverse(root,0);
-return ans;
+//     if(!root) return false;
+//     let ans=false;
+//     let traverse=(curr,currSum)=>{
+//         let newSum=currSum+curr.val;
+//         if(!curr.left && !curr.right){
+//             if(newSum === targetSum){
+//                 ans=ans || true;
+//             }
+//         }
+//             curr.left && traverse(curr.left,newSum);
+//     curr.right && traverse(curr.right,newSum); 
+//     }
+// traverse(root,0);
+// return ans;
+if(!root) return false;
+if(!root.left && !root.right){
+    return root.val ===targetSum
+}
+let leftSubTreeHasPathSum=hasPathSum(root.left,targetSum-root.val);
+let rightSubTreeHasPathSum=hasPathSum(root.right,targetSum-root.val);
+
+return leftSubTreeHasPathSum || rightSubTreeHasPathSum
 };
